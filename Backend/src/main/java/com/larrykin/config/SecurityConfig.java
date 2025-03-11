a593b2bda3c59c9c894148469546dab6d5b031ef
Kinuthia Lawrence <kinuthialawrence343@gmail.com>
2025-03-11 06:59:28 +0300

    added a docker file

--- Backend/src/main/java/com/larrykin/config/SecurityConfig.java
+++ Backend/src/main/java/com/larrykin/config/SecurityConfig.java
@@ -1,8 +1,29 @@
 package com.larrykin.config;
 
+import com.larrykin.jwt.AuthEntryPoint;
+import com.larrykin.jwt.AuthTokenFilter;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.AuthorizeHttpRequestsConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+
+import java.util.Arrays;
+import java.util.Collections;
 
 @Configuration
 @EnableWebSecurity
@@ -12,4 +33,54 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
         jsr250Enabled = true
 )
 public class SecurityConfig {
+    @Autowired
+    private AuthTokenFilter authTokenFilter;
+    @Autowired
+    private AuthEntryPoint authEntryPoint;
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.authorizeHttpRequests((request) ->
+                ((AuthorizeHttpRequestsConfigurer.AuthorizedUrl) request.anyRequest()).permitAll());
+        httpSecurity.formLogin(Customizer.withDefaults());
+        httpSecurity.httpBasic(Customizer.withDefaults());
+        httpSecurity.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+        httpSecurity.addFilterBefore(authTokenFilter, UsernamePasswordAuthenticationFilter.class);
+        //add jwt token validator filter
+        httpSecurity.exceptionHandling(exception -> exception.authenticationEntryPoint(authEntryPoint));
+        httpSecurity.csrf(AbstractHttpConfigurer::disable);
+        httpSecurity.cors(cors -> cors.configurationSource(corsConfigurationSource()));
+
+        return httpSecurity.build();
+    }
+
+    private CorsConfigurationSource corsConfigurationSource() {
+        return new CorsConfigurationSource() {
+            @Override
+            public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
+                CorsConfiguration corsConfiguration = new CorsConfiguration();
+                corsConfiguration.setAllowedOrigins(Arrays.asList(
+                        "http://localhost:5173",
+                        "http://localhost:5174",
+                        "http://localhost:5175"
+                ));
+                corsConfiguration.setAllowedMethods((java.util.List<String>) Collections.singleton("*"));
+                corsConfiguration.setAllowCredentials(true);
+                corsConfiguration.setAllowedHeaders(Collections.singletonList("*"));
+                corsConfiguration.setExposedHeaders(Arrays.asList("Authorization"));
+                corsConfiguration.setMaxAge(3600L);
+                return corsConfiguration;
+            }
+        };
+    }
+
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean // automatically authenticates  the user from the db using authentication method and the provided credentials.
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
 }
